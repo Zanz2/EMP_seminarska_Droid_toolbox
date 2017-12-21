b414c8c514d3676f1ac6cda950861e8b689f2d4e
Zanz2 <zan.zagar@hotmail.com>
2017-12-21 15:23:34 +0100

    prvi commit veselja in sreče

---
+++ app/src/main/java/com/example/zanzagar/androidtoolbox_empsem/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.zanzagar.androidtoolbox_empsem;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
